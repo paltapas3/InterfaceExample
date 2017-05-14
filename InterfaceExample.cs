1996719885dfa33603b9d71e667d1392b379141f
paltapas3 <paltapas3@gmail.com>
2017-05-14 12:31:31 +0530

    Sample C# Console application to demonstrate how interfece could be implemented in a C# class

---
+++ InterfaceExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    interface IMyInterface
+    {
+        void MethodToImplement();//Abstract Method signature.
+    }
+
+    class InterfaceExample : IMyInterface
+    {
+        static void Main(string[] args)
+        {
+            InterfaceExample obj = new InterfaceExample();
+            obj.MethodToImplement();
+        }
+
+        public void MethodToImplement()
+        {
+            //Abstract Method Implementation
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
